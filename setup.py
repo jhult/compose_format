d47eade795c0b00f96ea389eedc8ac130624b56d
stefan <think@hotmail.de>
2016-08-14 12:17:50 +0200

    release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='compose_format',
-    version='0.1.6',
+    version='0.2.0',
     description='format docker-compose files',
     long_description=readme(),
     url='http://github.com/funkwerk/compose_format',
